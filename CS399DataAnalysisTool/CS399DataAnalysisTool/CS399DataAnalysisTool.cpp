b4ee014357bc137c33d7e81a8bb75d662441083f
BarBenzvi <65105160+BarBenzvi@users.noreply.github.com>
2022-04-06 20:45:17 -0700

    Added comment for steps

--- CS399DataAnalysisTool/CS399DataAnalysisTool/CS399DataAnalysisTool.cpp
+++ CS399DataAnalysisTool/CS399DataAnalysisTool/CS399DataAnalysisTool.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
+/*
+* Accept directory to analyze
+* Get all header files in directory (including sub-directories)
+* Analyze each file looking for classes
+* Analyze each class - find data size (number of variables) and number of functions
+* Store data found in some kind of struct/array
+* Serialize data to json file
+*/
+
 int main()
 {
     std::cout << "Hello World!\n";
